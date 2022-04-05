8438403c0463e18651c574b3d0ff3afd80ffc0f7
leomitx10 <90487905+leomitx10@users.noreply.github.com>
2022-04-04 22:09:30 -0300

    Delete src directory

--- src/backend/urls.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-from backend.todo.router import router
-
-urlpatterns = [path("admin/", admin.site.urls), path("api/", include(router.urls))]
